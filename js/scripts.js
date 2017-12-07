c25c9377830fd905ecd3e40f92ab9c4402d2b49b
Arnold Sztandarski <a.sztandarski@wp.pl>
2017-12-07 08:36:30 +0100

    add files

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+function downloadElements() {
+	var buttons = document.getElementsByClassName('button');
+	console.log(buttons);
+
+	var max = buttons.length;
+
+	for(i = 1; i <= max; i++) {
+		alert(innerText = buttons);
+	}
+}
+
+downloadElements();
+
